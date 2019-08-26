8372b350b2a85ef8fc22b7a0b2f2116e97cdf6c6
jwy5140 <jwy5140@gmail.com>
2019-08-25 21:40:44 -0400

    Custom error messages for validations added.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
     has_one :network
     has_many :network_goals, through: :network, source: :partner, source_type: 'Goal'
 
-    validates :username, uniqueness: { case_sensitive: true }, length: { minimum: 4, maximum: 14 }
-    validates :age, exclusion: { in: 0..18 }
-    validates :email, format: { with: /\S+@[a-z]+.com/i }
+    validates :username, uniqueness: { case_sensitive: true, message: 'Username already exists.' }, length: { minimum: 4, maximum: 14 }
+    validates :age, exclusion: { in: 0..18, message: 'You must be 18 years or older to use this app.' }
+    validates :email, format: { with: /\S+@[a-z]+.com/i, message: 'Incorrect email format.' }, uniqueness: { case_sensitive: false, message: 'There is already an account associated with this email.' }
     validates :bio, length: { maximum: 180 }
-    validates :location, format: { with: /[0-9]{5}/ }
+    validates :location, format: { with: /[0-9]{5}/, message: 'Please only enter 5 numbers for zip code.' }
     
 end
