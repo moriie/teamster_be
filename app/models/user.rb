66835d530007ab664954291a54ab1970e4568042
jwy5140 <jwy5140@gmail.com>
2019-10-15 04:45:24 -0400

    Finally fixed age validation to check for only integer, and greater than 18.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
     has_many :network_goals, through: :network, source: :partner, source_type: 'Goal'
 
     validates :username, uniqueness: { case_sensitive: true }, length: { minimum: 4, maximum: 14 }
-    validates :age, exclusion: { in: 0..18, message: 'must be 18 or older to use this application.' }
-    validates :age, numericality: true
+    validates :age, numericality: { only_integer: true, message: 'must be an number.' }
+    validates :age, numericality: { greater_than: 17, message: 'must be 18 or older to use this application.' }
     validates :email, format: { with: /\S+@[a-z]+.com/i }, uniqueness: { case_sensitive: false }
     validates :bio, length: { maximum: 180 }
     validates :location, format: { with: /[0-9]{5}/, message: 'must be standard 5 digit US zip code' }
