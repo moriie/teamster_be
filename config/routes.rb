c6a4c7be2ba56810d21d6dc2fbc198208ee6100e
jwy5140 <jwy5140@gmail.com>
2019-10-25 02:02:01 -0400

    Create routes for ignore and block actions.

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create, :destroy]
   resources :users
   resources :goals
+  resources :ignores, only: [:create]
+  resources :blocks, only: [:create, :destroy]
 
   post 'signup', to: 'users#create', as: 'signup'
   post 'login', to: 'sessions#create', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'ignores/:id/destroy_all', to: 'ignores#destroy_all'
 
   get 'session', to: 'sessions#auth_check'
   get 'session_end', to: 'sessions#destroy'
